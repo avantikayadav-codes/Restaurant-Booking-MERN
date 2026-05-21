4e46364b5cd44322bb86e74a07ff27b282bab0e4
Avantika Yadav <avantikayadav632@gmail.com>
2026-05-21 11:35:43 +0530

    Added Restaurant Booking MERN Project

---
+++ Backend/controller/reservation.js
@@ -0,0 +1,33 @@
+import ErrorHandler from "../error/error.js";
+import { Reservation } from "../models/reservationSchema.js";
+
+export const sendReservation = async (req, res, next) => {
+  const { firstName, lastName, email, phone, time, date } = req.body;
+  if (!firstName || !lastName || !email || !phone || !time || !date) {
+    return next(new ErrorHandler("Please Fill Full Reservation Form!", 400));
+  }
+
+  try {
+    await Reservation.create({
+      firstName, 
+      lastName, 
+      email, 
+      phone, 
+      time, 
+      date
+    });
+    res.status(201).json({
+      success: true,
+      message: "Reservation Sent Successfully!",
+    });
+  } catch (error) {
+    // Handle Mongoose validation errors
+    if (error.name === "ValidationError") {
+      const validationErrors = Object.values(error.errors).map(err => err.message);
+      return next(new ErrorHandler(validationErrors.join(' , '), 400));
+    }
+    return next(error);
+  }
+};
+
+
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">AVANTIKA</div>
+          <div className="right">
+            <p>Scheme-78, Vijay Nagar, Indore</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By AVANTIKA YADAV</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Avantika.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
